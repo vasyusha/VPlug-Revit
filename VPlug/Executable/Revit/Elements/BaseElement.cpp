82e47a717a1ff9f67fb47f31f7b9775a59dd63a0
vasyusha <vasyusha7@gmail.com>
2025-09-01 11:43:33 +0300

    add file BaseElement.h/.cpp

---
+++ VPlug/Executable/Revit/Elements/BaseElement.cpp
@@ -0,0 +1,36 @@
+﻿#include "..\..\..\Headlines\Revit\Elements\BaseElement.h"
+
+void Elements::BaseElement::SetId(String^ id) {
+	id_ = id;
+}
+String^ Elements::BaseElement::GetId() {
+	return id_;
+}
+
+void Elements::BaseElement::SetName(String^ name) {
+	name_ = name;
+}
+String^ Elements::BaseElement::GetName() {
+	return name_;
+}
+
+void Elements::BaseElement::SetGuid(String^ guid) {
+	guid_ = guid;
+}
+String^ Elements::BaseElement::GetGuid() {
+	return guid_;
+}
+
+void Elements::BaseElement::SetParameters(String^ key, String^ value) {
+	if(parameters_ == nullptr) {
+		parameters_ = gcnew Dictionary<String^, String^>();
+	}
+
+	if(key != nullptr && value != nullptr) {
+		parameters_->Add(key, value);
+	}
+}
+Dictionary<String^, String^>^ Elements::BaseElement::GetParameters() {
+	return parameters_;
+}
+
---
+++ VPlug/Headlines/Revit/Elements/BaseElement.h
@@ -0,0 +1,29 @@
+#pragma once
+
+using namespace System;
+using namespace System::Collections::Generic;
+
+namespace Elements {
+
+public ref class BaseElement {
+private:
+	String^ id_;
+	String^ name_;
+	String^ guid_;
+
+	Dictionary<String^, String^>^ parameters_;
+public:
+	virtual void SetId(String^ id);
+	virtual String^ GetId();
+
+	virtual void SetName(String^ name);
+	virtual String^ GetName();
+
+	virtual void SetGuid(String^ guid);
+	virtual String^ GetGuid();
+
+	virtual void SetParameters(String^ key, String^ value);
+	virtual Dictionary<String^, String^>^ GetParameters();
+};
+
+}
