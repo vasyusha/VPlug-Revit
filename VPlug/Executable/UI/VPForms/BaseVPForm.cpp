4eabdb195e372b78d5544ad3fd6dc358e967e47f
vasyusha <vasyusha7@gmail.com>
2025-09-02 09:02:02 +0300

    add file BaseVPForm.h/.cpp

---
+++ VPlug/Executable/UI/VPForms/BaseVPForm.cpp
@@ -0,0 +1,5 @@
+﻿#include "..\..\..\Headlines\UI\VPForms\BaseVPForm.h"
+
+void VPForms::BaseVPForm::OnClose(Object^ sender, EventArgs^ e) {
+	this->Close();
+}
---
+++ VPlug/Headlines/UI/VPForms/BaseVPForm.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+using namespace System;
+using namespace System::Windows::Forms;
+namespace VPForms {
+
+public ref class BaseVPForm : public Form {
+public:
+	void OnClose(Object^ sender, EventArgs^ e);
+};
+
+}
