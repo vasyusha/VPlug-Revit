7290569d278b470e20ce4caf816b7781a0e00cc3
vasyusha <vasyusha7@gmail.com>
2025-09-08 18:14:06 +0300

    add file ParameterFilledFilter.h/.cpp

---
+++ VPlug/Executable/Revit/Filters/ParameterFilledFilter.cpp
@@ -0,0 +1,52 @@
+﻿#include "..\..\..\Headlines\Revit\Filters\ParameterFilledFilter.h"
+
+String^ Filters::ParameterFilledFilter::CheckParam(Document^ doc, Element^ element, String^ parameter) {
+	if(element == nullptr) return nullptr;
+
+	Parameter^ find_parameter = element->LookupParameter(parameter);
+
+	if(find_parameter == nullptr) {
+		//!!!TESTs!!! тест конструкции
+		try {
+			
+		} catch(...) { 
+			//NullReferenceException - если элемент не имеет типа
+			//InvalidOperationException - если тип недоступен или элемент не поддерживает типизацию
+			//ArgumentException - если попытка получить тип у неверного объекта
+			Element^ element_type = doc->GetElement(element->GetTypeId());
+			find_parameter = element_type->LookupParameter(parameter);
+		}
+	}
+
+	String^ value = nullptr;
+
+	if(find_parameter != nullptr) {
+		switch (find_parameter->StorageType) {
+			case StorageType::Double :
+				value = find_parameter->AsValueString();
+				break;
+			case StorageType::Integer : 
+				value = find_parameter->AsInteger().ToString();
+				break;
+			case StorageType::String :
+				value = find_parameter->AsString();
+				break;
+			case StorageType::ElementId :
+				value = find_parameter->AsElementId()->IntegerValue.ToString();
+				break;
+			default:
+				value = "Ошибка, неизвестный элемент";
+				break;
+		}
+	}
+
+	if(find_parameter != nullptr) {
+		if(!find_parameter->HasValue) {
+			value = "Ошибка - параметер не заполнен!";
+		}
+	} else {
+		value = "Ошибка - Параметр отсутствует у семейства типа/экземаляра";
+	}
+	
+	return value;
+}
---
+++ VPlug/Headlines/Revit/Filters/ParameterFilledFilter.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+using namespace Autodesk::Revit::UI;
+using namespace Autodesk::Revit::DB;
+using namespace Autodesk::Revit::Attributes;
+
+using namespace System;
+using namespace System::Collections::Generic;
+
+namespace Filters {
+
+public ref class ParameterFilledFilter {
+public:
+	String^ CheckParam(Document^ doc, Element^ element, String^ parameter);
+
+};
+
+}
