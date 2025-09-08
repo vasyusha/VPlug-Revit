689190caa40564e70a0ebe5df8fd3c05d0c4b5da
vasyusha <vasyusha7@gmail.com>
2025-09-08 17:25:45 +0300

    add file ElementCollectorFilter.h/.cpp

---
+++ VPlug/Executable/Revit/Filters/ElementCollectorFilter.cpp
@@ -0,0 +1,43 @@
+﻿#include "..\..\..\Headlines\Revit\Filters\ElementCollectorFilter.h"
+
+Filters::ElementCollectorFilter::ElementCollectorFilter(Document^ doc, BuiltInCategory category) 
+	: doc_(doc), category_(category) {
+
+	class_type_ = nullptr;
+	category_id_ = nullptr;
+}
+
+Filters::ElementCollectorFilter::ElementCollectorFilter(Document^ doc, Type^ class_type) : doc_(doc)
+	,class_type_(class_type) {
+	
+	category_id_ = nullptr;
+}
+
+Filters::ElementCollectorFilter::ElementCollectorFilter(Document^ doc, int category_id) : doc_(doc) {
+	
+	class_type_ = nullptr;
+	category_id_ = gcnew ElementId(category_id);
+}
+
+List<Element^>^ Filters::ElementCollectorFilter::FindElements() {
+	List<Element^>^ result = gcnew List<Element^>();
+
+	FilteredElementCollector^ collector = gcnew FilteredElementCollector(doc_);
+
+	if(class_type_ != nullptr) {
+		collector->OfClass(class_type_);
+	} else if(category_id_ != nullptr) {
+		collector->OfCategoryId(category_id_);
+	} else {
+		collector->OfCategory(category_);
+	}
+
+	collector->WhereElementIsNotElementType();
+
+	for each(Element^ e in collector) {
+		result->Add(e);
+	}
+
+	return result;
+}
+
---
+++ VPlug/Headlines/Revit/Filters/ElementCollectorFilter.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+using namespace Autodesk::Revit::UI;
+using namespace Autodesk::Revit::DB;
+using namespace Autodesk::Revit::Attributes;
+
+using namespace System;
+using namespace System::Collections::Generic;
+
+namespace Filters {
+
+public ref class ElementCollectorFilter {
+private:
+	Document^ doc_;
+	BuiltInCategory category_;
+	Type^ class_type_;
+	ElementId^ category_id_;
+
+public:
+	ElementCollectorFilter(Document^ doc, BuiltInCategory category);
+	ElementCollectorFilter(Document^ doc, Type^ class_type);
+	ElementCollectorFilter(Document^ doc, int category_id);
+
+	List<Element^>^ FindElements();
+
+};
+
+}
