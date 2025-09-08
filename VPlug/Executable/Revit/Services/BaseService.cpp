cc0c8096666b9b8c286c871642801adedbeb1852
vasyusha <vasyusha7@gmail.com>
2025-09-08 18:15:37 +0300

    add file BaseService.h/.cpp

---
+++ VPlug/Executable/Revit/Services/BaseService.cpp
@@ -0,0 +1,9 @@
+﻿#include "..\..\..\Headlines\Revit\Services\BaseService.h"
+
+Services::BaseService::BaseService(Document^ doc, int category_id, List<String^>^ parameters) 
+	: doc_(doc) {
+	
+	Filters::ElementCollectorFilter^ collector = gcnew Filters::ElementCollectorFilter(doc, category_id);
+	List<Element^>^ elements = collector->FindElements();
+
+}
---
+++ VPlug/Headlines/Revit/Services/BaseService.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include "..\Filters\ElementCollectorFilter.h"
+
+using namespace Autodesk::Revit::UI;
+using namespace Autodesk::Revit::DB;
+using namespace Autodesk::Revit::Attributes;
+
+using namespace System;
+using namespace System::Collections::Generic;
+
+namespace Services {
+
+public ref class BaseService {
+private:
+	Document^ doc_;
+
+public:
+	BaseService(Document^ doc, int category_id, List<String^>^ parameters);
+	
+};
+
+}
