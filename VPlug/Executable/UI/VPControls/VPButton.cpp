cd2738a7e757bffd6b8acf5e3c2128438a72cfe7
vasyusha <vasyusha7@gmail.com>
2025-10-20 13:23:34 +0300

    deleted file VPBaseControl.h/.cpp VPButton.h/.cpp BaseVPForm.h/.cpp VPFormAuditParameterCommand.h/.cpp

--- VPlug/Executable/UI/VPControls/VPButton.cpp
+++
@@ -1,5 +0,0 @@
-﻿#include "..\..\..\Headers\UI\VPControls\VPButton.h"
-
-VPControls::VPButton::VPButton(String^ name, int x, int y, int width, int height) 
-	: VPBaseControl(name, x, y, width, height) {
-}
--- VPlug/Executable/UI/VPForms/BaseVPForm.cpp
+++
@@ -1,5 +0,0 @@
-﻿#include "..\..\..\Headers\UI\VPForms\BaseVPForm.h"
-
-void VPForms::BaseVPForm::OnClose(Object^ sender, EventArgs^ e) {
-	this->Close();
-}
