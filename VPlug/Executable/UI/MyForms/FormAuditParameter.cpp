b89f60befdd40a5a16e0fc8a56e2249ca77aed93
vasyusha <vasyusha7@gmail.com>
2025-10-21 17:15:48 +0300

    feat(FormAuditParameter): проработка новой логики

--- VPlug/Executable/UI/MyForms/FormAuditParameter.cpp
+++ VPlug/Executable/UI/MyForms/FormAuditParameter.cpp
@@ -1,1 +1,19 @@
-﻿
+﻿#include "../../../Headers/UI/MyForms/FormAuditParameter.h"
+
+namespace MyForm {
+
+FormAuditParameter::FormAuditParameter() {
+	this->Text = "Проверка параметров";
+	this->StartPosition = FormStartPosition::CenterScreen;
+	this->Size = Drawing::Size(1000, 700);
+
+}
+
+void FormAuditParameter::BuildUi() {
+	this->SuspendLayout();
+	
+
+	this->ResumeLayout(false);
+}
+
+}//namespace MyForm
--- VPlug/Headers/UI/MyForms/FormAuditParameter.h
+++ VPlug/Headers/UI/MyForms/FormAuditParameter.h
@@ -1,1 +1,59 @@
-﻿
+﻿#pragma once
+
+using namespace System;
+using namespace System::Collections::Generic;
+using namespace System::Windows::Forms;
+using namespace System::Drawing;
+
+namespace MyForm {
+
+enum class AuditStage {
+	None,
+	MehtodSelected,
+	ConfigLoaded,
+	ChecksSelected,
+	Running,
+	RunningDone,
+	Export,
+	ExportDone
+};
+
+public ref class AuditSummaryRow {
+public:
+	property String^ Scope;
+	property String^ Name;
+	property int Total;
+	property int Passed;
+	property int Failed;
+	property int MissedValue;
+	property int NoParam;
+};
+
+public delegate void AuditMethodSelectedHandler(String^ method);
+public delegate void AuditConfigLoadedHandler(String^ path);
+public delegate void AuditChecksSelectedHandler();
+public delegate void AuditRunningHandler();
+public delegate void AuditExportHandler(String^ path);
+
+public ref class FormAuditParameter : public Windows::Forms::Form{
+private:
+	AuditStage auditStage;
+	
+	String^ selectedMethod_;
+	String^ configPath_;
+	String^ exportPath_;
+
+	List<AuditSummaryRow^>^ summaryRows_;
+
+	void BuildUi();
+
+
+public:
+	FormAuditParameter();
+
+};
+
+
+
+
+}//namespace MyForm
