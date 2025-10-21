3522ec0051e509a90c717ee12d916f7c370c55a2
vasyusha <vasyusha7@gmail.com>
2025-10-21 17:15:11 +0300

    feat(AuditParameterCommand): проработка новой логики

--- VPlug/Executable/Commands/AuditParameterCommand.cpp
+++ VPlug/Executable/Commands/AuditParameterCommand.cpp
@@ -1,1 +1,17 @@
-﻿
+﻿#include "../../Headers/Commands/AuditParameterCommand.h"
+
+namespace Commands {
+
+	Result AuditParameterCommand::Execute(ExternalCommandData^ commandData,
+		String^% message, ElementSet^ elements) {
+
+		UIDocument^ uiDoc = commandData->Application->ActiveUIDocument;
+		doc_ = uiDoc->Document;
+		
+		form_ = gcnew MyForm::FormAuditParameter();
+
+		form_->ShowDialog();
+
+		return Result::Succeeded;
+	}
+}
--- VPlug/Headers/Commands/AuditParameterCommand.h
+++ VPlug/Headers/Commands/AuditParameterCommand.h
@@ -1,1 +1,27 @@
-﻿
+﻿#pragma once
+
+#include "../../Headers/UI/MyForms/FormAuditParameter.h"
+
+using namespace System;
+using namespace System::Drawing;
+using namespace System::Windows::Forms;
+using namespace System::Collections::Generic;
+
+using namespace Autodesk::Revit::UI;
+using namespace Autodesk::Revit::DB;
+using namespace Autodesk::Revit::Attributes;
+
+namespace Commands {
+
+[Transaction(TransactionMode::Manual)]
+public ref class AuditParameterCommand : public IExternalCommand {
+private:
+	Document^ doc_;
+	MyForm::FormAuditParameter^ form_;
+
+public:
+	virtual Result Execute(ExternalCommandData^ commandData,
+		String^% message, ElementSet^ elements);
+};
+
+}
