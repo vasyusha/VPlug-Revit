b0aee21f5591ef562b04b2d3a0e9f97dea229e5d
vasyusha <vasyusha7@gmail.com>
2025-10-27 17:40:42 +0300

    feat(AuditParameterExportHtml): добавил реализацию через property и конструкторы

--- VPlug/Executable/Export/html/AuditParameterExportHtml.cpp
+++ VPlug/Executable/Export/html/AuditParameterExportHtml.cpp
@@ -1,1 +1,129 @@
-﻿
+﻿#include "../../../Headers/Export/html/AuditParameterExportHtml.h"
+
+namespace ExportHtml {
+
+RequirementRow::RequirementRow() {
+	Rows = gcnew List<Row^>();
+}
+
+CategoryReport::CategoryReport() {
+	Requirements = gcnew List<RequirementRow^>();
+}
+
+String^ CategoryReport::Slug::get() {
+	return slug_;
+}
+
+void CategoryReport::Slug::set(String^ value) {
+	if (value != nullptr) {
+		slug_ = value->Replace(' ', '_');
+	} else {
+		slug_ = nullptr;
+	}
+}
+
+int CategoryReport::ChecksPass::get() {
+	return checksPass_;
+}
+
+void CategoryReport::ChecksPass::set(int value) {
+	checksPass_ = value;
+}
+
+int CategoryReport::ChecksTotal::get() {
+	return checksTotal_;
+}
+
+void CategoryReport::ChecksTotal::set(int value) {
+	checksTotal_ = value;
+}
+
+int CategoryReport::ElementsPass::get() {
+	return elementsPass_;
+}
+
+void CategoryReport::ElementsPass::set(int value) {
+	elementsPass_ = value;
+}
+
+int CategoryReport::ElementsTotal::get() {
+	return elementsTotal_;
+}
+
+void CategoryReport::ElementsTotal::set(int value) {
+	elementsTotal_ = value;
+}
+
+int CategoryReport::Percent::get() {
+	return percent_;
+}
+
+void CategoryReport::Percent::set(int value) {
+	if (value < 0) value = 0;
+	if (value > 100) value = 100;
+	percent_ = value;
+}
+
+ReportModel::ReportModel() {
+	Categories = gcnew List<CategoryReport^>();
+}
+
+int ReportModel::ReqPass::get() {
+	return reqPass_;
+}
+
+void ReportModel::ReqPass::set(int value) {
+	reqPass_ = value;
+}
+
+int ReportModel::ReqTotal::get() {
+	return reqTotal_;
+}
+
+void ReportModel::ReqTotal::set(int value) {
+	reqTotal_ = value;
+}
+
+int ReportModel::CheckPass::get() {
+	return checkPass_;
+}
+
+void ReportModel::CheckPass::set(int value) {
+	checkPass_ = value;
+}
+
+int ReportModel::CheckTotal::get() {
+	return checkTotal_;
+}
+
+void ReportModel::CheckTotal::set(int value) {
+	checkTotal_ = value;
+}
+
+int ReportModel::ElPass::get() {
+	return elPass_;
+}
+
+void ReportModel::ElPass::set(int value) {
+	elPass_ = value;
+}
+
+int ReportModel::ElTotal::get() {
+	return elTotal_;
+}
+
+void ReportModel::ElTotal::set(int value) {
+	elTotal_ = value;
+}
+
+int ReportModel::Percent::get() {
+	return percent_;
+}
+
+void ReportModel::Percent::set(int value) {
+	if (value < 0) value = 0;
+	if (value > 100) value = 100;
+	percent_ = value;
+}
+
+}// namespace ExportHtml
--- VPlug/Headers/Export/html/AuditParameterExportHtml.h
+++ VPlug/Headers/Export/html/AuditParameterExportHtml.h
@@ -8,8 +8,10 @@ namespace ExportHtml {
 
 public ref class RequirementRow {
 public:
-	String^ Title;
-	bool Pass;
+	RequirementRow();
+
+	property String^ Title;
+	property bool Pass;
 
 	ref struct Row {
 		int Id;
@@ -17,33 +19,108 @@ public:
 		String^ Comment;
 	};
 
-	List<Row^> Rows;
+	List<Row^>^ Rows;
 };
 
 public ref class CategoryReport {
+private:
+	int checksPass_;
+	int checksTotal_;
+	int elementsPass_;
+	int elementsTotal_;
+	int percent_;
+	String^ slug_;
+
 public:
-	String^ Name;
-	String^ Slug;
-	int ChecksPass;
-	int ChecksTotal;
-	int ElementsPass;
-	int ElementsTotal;
-	int Percent;
+	CategoryReport();
+
+	property String^ Name;
+
+	property String^ Slug {
+		String^ get();
+		void set(String^ value);
+	};
+
+	property int ChecksPass {
+		int get();
+		void set(int value);
+	};
+
+	property int ChecksTotal {
+		int get();
+		void set(int value);
+	};
+
+	property int ElementsPass {
+		int get();
+		void set(int value);
+	};
+
+	property int ElementsTotal {
+		int get();
+		void set(int value);
+	};
+
+	property int Percent {
+		int get();
+		void set(int value);
+	};
+
 	List<RequirementRow^>^ Requirements;
 };
 
-public ref class ReportMode {
+public ref class ReportModel {
+private:
+	int reqPass_;
+	int reqTotal_;
+	int checkPass_;
+	int checkTotal_;
+	int elPass_;
+	int elTotal_;
+	int percent_;
+
 public:
-	String^ FilePath;
-	String^ ProjectName;
-	String^ DataTime;
-	int ReqPass;
-	int ReqTotal;
-	int CheckPass;
-	int CheckTotal;
-	int ElPass;
-	int ElTotal;
-	int Percent;
+	ReportModel();
+
+	property String^ FilePath;
+	property String^ ProjectName;
+	property String^ DataTimeStr;
+
+	property int ReqPass {
+		int get();
+		void set(int value);
+	};
+
+	property int ReqTotal {
+		int get();
+		void set(int value);
+	};
+
+	property int CheckPass {
+		int get();
+		void set(int value);
+	};
+
+	property int CheckTotal {
+		int get();
+		void set(int value);
+	};
+
+	property int ElPass {
+		int get();
+		void set(int value);
+	};
+
+	property int ElTotal {
+		int get();
+		void set(int value);
+	};
+
+	property int Percent {
+		int get();
+		void set(int value);
+	};
+
 	List<CategoryReport^>^ Categories;
 };
 
