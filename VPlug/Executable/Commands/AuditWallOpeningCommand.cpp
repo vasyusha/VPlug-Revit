17b80dcbf32f5faa82e251be60fdfc1175463d44
vasyusha <vasyusha7@gmail.com>
2025-11-17 14:37:00 +0300

    feat(AuditWallOpeningCommand): добавли логику инициализации команды

--- VPlug/Executable/Commands/AuditWallOpeningCommand.cpp
+++ VPlug/Executable/Commands/AuditWallOpeningCommand.cpp
@@ -1,1 +1,18 @@
-﻿
+﻿#include "../../Headers/Commands/AuditWallOpeningCommand.h"
+
+namespace Commands {
+
+Result AuditWallOpeningCommand::Execute(ExternalCommandData^ commandData,
+	String^% message, ElementSet^ elements) {
+
+	UIDocument^ uiDoc = commandData->Application->ActiveUIDocument;
+	doc_ = uiDoc->Document;
+	
+	form_ = gcnew MyForm::FormAuditWallOpening();
+	
+	form_->ShowDialog();
+
+	return Result::Succeeded;
+}
+
+}//namespace Commands
--- VPlug/Headers/Commands/AuditWallOpeningCommand.h
+++ VPlug/Headers/Commands/AuditWallOpeningCommand.h
@@ -1,1 +1,37 @@
-﻿
+﻿#pragma once
+
+#include "../../Headers/UI/MyForms/FormAuditWallOpening.h"
+
+using namespace System;
+using namespace System::Drawing;
+using namespace System::Windows::Forms;
+using namespace System::Collections::Generic;
+
+using namespace Autodesk::Revit::UI;
+using namespace Autodesk::Revit::DB;
+using namespace Autodesk::Revit::Attributes;
+
+namespace Commands {
+
+[Transaction(TransactionMode::Manual)]
+public ref class AuditWallOpeningCommand : public IExternalCommand {
+private:
+	Document^ doc_;
+	MyForm::FormAuditWallOpening^ form_;
+
+public:
+	virtual Result Execute(ExternalCommandData^ commandData,
+		String^% message, ElementSet^ elements);
+};
+
+}//namespace Commands
+
+
+
+
+
+
+
+
+
+
