a4ac05063e03af6568d96f877ef42847992fee66
vasyusha <vasyusha7@gmail.com>
2025-11-17 17:19:04 +0300

    add file WallElement.h/.cpp

---
+++ VPlug/Executable/Revit/Elements/WallElement.cpp
@@ -0,0 +1,21 @@
+﻿#include "..\..\..\Headers\Revit\Elements\WallElement.h"
+
+namespace Elements {
+
+double WallElement::Area::get() {
+	return area_;
+}
+
+void WallElement::Area::set(double value) {
+	area_ = value;
+}
+
+bool WallElement::HasOpening::get() {
+	return hasOpening_;
+}
+
+void WallElement::HasOpening::set(bool value) {
+	hasOpening_ = value;
+}
+
+}//namespace Elements
---
+++ VPlug/Headers/Revit/Elements/WallElement.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include "BaseElement.h"
+
+using namespace System;
+using namespace System::Collections::Generic;
+
+namespace Elements {
+
+public ref class WallElement : BaseElement {
+private:
+	double area_;
+	bool hasOpening_;
+
+public:
+
+	property double Area {
+		double get();
+		void set(double value);
+	};
+
+	property bool HasOpening {
+		bool get();
+		void set(bool value);
+	};
+};
+
+}//namespace Elements
