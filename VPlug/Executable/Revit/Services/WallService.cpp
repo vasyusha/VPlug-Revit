d500ed3f4b52e7555c8b4160b4474d6c5a142ede
vasyusha <vasyusha7@gmail.com>
2025-11-19 16:56:40 +0300

    feat(WallService): добавил функцию CountRealOpeningsForWall, проверяет реальный проём

--- VPlug/Executable/Revit/Services/WallService.cpp
+++ VPlug/Executable/Revit/Services/WallService.cpp
@@ -5,57 +5,131 @@ namespace Services {
 WallService::WallService(Document^ doc) : BaseService(doc) {
 }
 
+bool BoxesIntersect(BoundingBoxXYZ^ a, BoundingBoxXYZ^ b) {
+	if (a == nullptr || b == nullptr) return false;
+
+	XYZ^ aMin = a->Min;
+	XYZ^ aMax = a->Max;
+	XYZ^ bMin = b->Min;
+	XYZ^ bMax = b->Max;
+
+	bool overlapX = (aMin->X <= bMax->X) && (aMax->X >= bMin->X);
+	bool overlapY = (aMin->Y <= bMax->Y) && (aMax->Y >= bMin->Y);
+	bool overlapZ = (aMin->Z <= bMax->Z) && (aMax->Z >= bMin->Z);
+
+	return overlapX && overlapY && overlapZ;
+}
+
+int CountRealOpeningsForWall(Document^ doc, Wall^ wall) {
+	/*Получили список проёмов*/
+	IList<ElementId^>^ inserts = wall->FindInserts(
+		true,
+		true,
+		false,
+		false
+	);
+	
+	/*Получаем рамку геометрии стены*/
+	BoundingBoxXYZ^ wallBox = wall->BoundingBox[nullptr];
+	if (wallBox == nullptr)
+		return 0;
+
+	int count = 0;
+	
+	/*Перебор проёмов*/
+	for each (ElementId^ id in inserts) {
+		/*Получаем элемент*/
+		Element^ e = doc->GetElement(id);
+		if (e == nullptr)
+			continue;
+		
+		/*Получаем семейство*/
+		FamilyInstance^ fi = dynamic_cast<FamilyInstance^>(e);
+		if (fi == nullptr)
+			continue;
+
+		/*Получаем категорию семейства*/
+		Category^ cat = fi->Category;
+		if (cat == nullptr)
+			continue;
+
+		/*Фильтрация*/
+		BuiltInCategory bic = safe_cast<BuiltInCategory>(cat->Id->IntegerValue);
+		bool isOpeningFamily =
+			bic == BuiltInCategory::OST_Windows ||
+			bic == BuiltInCategory::OST_Doors ||
+			bic == BuiltInCategory::OST_SWallRectOpening ||
+			bic == BuiltInCategory::OST_GenericModel;
+		if (!isOpeningFamily)
+			continue;
+		
+		/*Создаём хост-стену для сверки*/
+		Wall^ hostWall = dynamic_cast<Wall^>(fi->Host);
+		if (hostWall == nullptr)
+			continue;
+		
+		/*Получаем рамку геометрии проёма*/
+		BoundingBoxXYZ^ boxIns = fi->BoundingBox[nullptr];
+		if (boxIns == nullptr)
+			continue;
+
+		/*1) Окно хостится в ЭТОЙ же стене*/
+		if (hostWall->Id == wall->Id) {
+			if (BoxesIntersect(wallBox, boxIns))
+				count++;
+			continue;
+		}
+
+		/*Окно хостится в ДРУГОЙ стене – shadow-кейс*/
+		BoundingBoxXYZ^ boxHost = hostWall->BoundingBox[nullptr];
+		if (boxHost == nullptr)
+			continue;
+
+		/*Нормаль хост-стены*/
+		XYZ^ n = hostWall->Orientation;
+
+		/*Центры bbox хост-стены и проверяемой стены*/
+		XYZ^ hostCenter = (boxHost->Min + boxHost->Max) * 0.5;
+		XYZ^ wallCenter = (wallBox->Min + wallBox->Max) * 0.5;
+
+		/*Проекции центров на нормаль*/
+		double hostN = hostCenter->DotProduct(n);
+		double wallN = wallCenter->DotProduct(n);
+
+		/*Сдвигаем проверяемую стену в сторону хоста по нормали*/
+		double dn = hostN - wallN;
+		XYZ^ shift = n * dn;
+
+		BoundingBoxXYZ^ movedWallBox = gcnew BoundingBoxXYZ();
+		movedWallBox->Min = wallBox->Min + shift;
+		movedWallBox->Max = wallBox->Max + shift;
+
+		/*Проверяем, пересекается ли "пододвинутая" стена с окном*/
+		if (!BoxesIntersect(movedWallBox, boxIns))
+			continue;
+
+		count++;
+	}
+	return count;
+}
+
 Elements::WallElement^ WallService::BuildWallElement(Document^ doc, Element^ e, IEnumerable<String^>^ requiredParams) {
 
 	Elements::WallElement^ we = Services::BaseService::BuildBaseElement<Elements::WallElement^>(doc, e, requiredParams);
 
 	Autodesk::Revit::DB::Wall^ wall = dynamic_cast<Autodesk::Revit::DB::Wall^>(e);
 
-
 	we->HasOpening = wall->FindInserts(true, true, false, false)->Count > 0;
-	
-	IList<ElementId^>^ rawInstens = wall->FindInserts(true, true, false, false);
 
-	int openingHosts = 0;
-	int openingShadows = 0;
-	for each (ElementId^ id in rawInstens) {
-		Element^ ins = doc->GetElement(id);
-		FamilyInstance^ fi = dynamic_cast<FamilyInstance^>(ins);
-		if (fi != nullptr) {
-			Wall^ hostWall = dynamic_cast<Wall^>(fi->Host);
-			if (hostWall != nullptr && hostWall->Id == wall->Id) {
-				++openingHosts;
-			} else {
-				++openingShadows;
-			}
-		} else {
-			++openingShadows;
-		}
-	}
-
-	we->CountOpening += openingHosts;
-	we->CountOpening += openingShadows;
-
-	/*
-	if (we->HasOpening) {
-		we->CountOpening = wall->FindInserts(true, true, false, false)->Count;
-	}
-
+	we->CountOpening = CountRealOpeningsForWall(doc, wall);
 	ForgeTypeId^ areaId = ParameterUtils::GetParameterTypeId(BuiltInParameter::HOST_AREA_COMPUTED);
 	Parameter^ areaParam = wall->GetParameter(areaId);
-	
-
 
 	if (areaParam != nullptr && areaParam->HasValue) {
 		double areaFt = areaParam->AsDouble();
 		double areaM2 = UnitUtils::ConvertFromInternalUnits(areaFt, UnitTypeId::SquareMeters);
 		we->Area = areaM2;
-
-	} else {
-		TaskDialog::Show("Area", "null");
-	}
-	*/
-
+	} 
 	return we;
 }
 
@@ -76,8 +150,5 @@ List<Elements::WallElement^>^ WallService::CollectByCategory(
 	}
 	return result;
 }
-
-
-
 
 }//namespace Services
