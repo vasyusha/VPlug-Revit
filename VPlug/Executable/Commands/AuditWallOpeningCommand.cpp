a48a2e69d31cd3b44727e4d6cbfe1046dded170c
vasyusha <vasyusha7@gmail.com>
2025-11-21 14:21:54 +0300

    feat(AuditWallOpeningCommand): перенес обработанные данные в MyDomain::WallOpening..., переделал передачу в данныех в ExportHtml

--- VPlug/Executable/Commands/AuditWallOpeningCommand.cpp
+++ VPlug/Executable/Commands/AuditWallOpeningCommand.cpp
@@ -2,138 +2,6 @@
 
 namespace Commands {
 
-String^ DataOpening::Name::get() {
-	return name_;
-}
-
-void DataOpening::Name::set(String^ value) {
-	name_ = value;
-}
-
-int DataOpening::Id::get() {
-	return id_;
-}
-
-void DataOpening::Id::set(int value) {
-	id_ = value;
-}
-
-String^ DataOpening::CategoryName::get() {
-	return categoryName_;
-}
-
-void DataOpening::CategoryName::set(String^ value) {
-	categoryName_ = value;
-}
-
-String^ AuditElementWall::Name::get() {
-	return name_;	
-}
-
-void AuditElementWall::Name::set(String^ value) {
-	name_ = value;
-}
-
-int AuditElementWall::Id::get() {
-	return id_;
-}
-
-void AuditElementWall::Id::set(int value) {
-	id_ = value;	
-}
-
-String^ AuditElementWall::CategoryName::get() {
-	return categoryName_;
-}
-
-void AuditElementWall::CategoryName::set(String^ value) {
-	categoryName_ = value;
-}
-
-double AuditElementWall::Area::get() {
-	return area_;
-}
-
-void AuditElementWall::Area::set(double value) {
-	area_ = value;
-}
-
-int AuditElementWall::CountOpening::get() {
-	return countOpening_;
-}
-
-void AuditElementWall::CountOpening::set(int value) {
-	countOpening_ = value;
-}
-
-IList<DataOpening^>^ AuditElementWall::DataOpenings::get() {
-	return dataOpenings_;
-}
-
-void AuditElementWall::DataOpenings::set(IList<DataOpening^>^ value) {
-	dataOpenings_ = value;
-}
-
-AuditTypeWallSummary::AuditTypeWallSummary() {
-	filters_ = gcnew Dictionary<String^, String^>();
-}
-
-String^ AuditTypeWallSummary::Name::get() {
-	return name_;
-}
-
-void AuditTypeWallSummary::Name::set(String^ value) {
-	name_ = value;
-}
-
-int AuditTypeWallSummary::TotalWalls::get() {
-	return totalWalls_;
-}
-
-void AuditTypeWallSummary::TotalWalls::set(int value) {
-	totalWalls_ = value;
-}
-
-double AuditTypeWallSummary::TotalArea::get() {
-	return totalArea_;
-}
-
-void AuditTypeWallSummary::TotalArea::set(double value) {
-	totalArea_ = value;
-}
-
-int AuditTypeWallSummary::TotalOpening::get() {
-	return totalOpening_;
-}
-
-void AuditTypeWallSummary::TotalOpening::set(int value) {
-	totalOpening_ = value;
-}
-
-double AuditTypeWallSummary::TotalAreaWallsWithOpenings::get() {
-	return totalAreaWallsWithOpenings_;
-}
-
-void AuditTypeWallSummary::TotalAreaWallsWithOpenings::set(double value) {
-	totalAreaWallsWithOpenings_ = value;
-}
-
-double AuditTypeWallSummary::TotalAreaWallsWithoutOpenings::get() {
-	return totalAreaWallsWithoutOpenings_;
-}
-
-void AuditTypeWallSummary::TotalAreaWallsWithoutOpenings::set(double value) {
-	totalAreaWallsWithoutOpenings_ = value;
-}
-
-IList<DataOpening^>^ AuditTypeWallSummary::DataOpenings::get() {
-	return dataOpenings_;
-}
-
-void AuditTypeWallSummary::DataOpenings::set(IList<DataOpening^>^ value) {
-	dataOpenings_ = value;
-}
-
 Result AuditWallOpeningCommand::Execute(ExternalCommandData^ commandData,
 	String^% message, ElementSet^ elements) {
 
@@ -141,7 +9,7 @@ Result AuditWallOpeningCommand::Execute(ExternalCommandData^ commandData,
 	doc_ = uiDoc->Document;
 	
 	form_ = gcnew MyForm::FormAuditWallOpening();
-
+	report = gcnew MyDomain::WallOpening::WallOpeningReport();
 	SubscriptionEvent();
 	
 	form_->ShowDialog();
@@ -152,6 +20,56 @@ Result AuditWallOpeningCommand::Execute(ExternalCommandData^ commandData,
 void AuditWallOpeningCommand::SubscriptionEvent() {
 	form_->RunRequest += gcnew MyForm::AuditWallOpeningRunningHandler(this, &AuditWallOpeningCommand::Audit);
 	form_->ExportRequest += gcnew MyForm::AuditWallOpeningExportHandler(this, &AuditWallOpeningCommand::Export);
+}
+
+void AuditWallOpeningCommand::PrepareElement(Elements::WallElement^ wallElement) {
+	MyDomain::WallOpening::WallElementInfo^ wallElementInfo = gcnew MyDomain::WallOpening::WallElementInfo();
+	
+	wallElementInfo->Id = wallElement->Id;
+	wallElementInfo->Name = wallElement->Name;
+	wallElementInfo->CategoryName = wallElement->CategoryName;
+	wallElementInfo->Area = wallElement->Area;
+	wallElementInfo->CountOpenings = wallElement->CountOpening;
+
+	for each (Elements::BaseElement^ baseElement in wallElement->LogicChildren) {
+		MyDomain::WallOpening::OpeningInfo^ openingInfo = gcnew MyDomain::WallOpening::OpeningInfo();
+
+		openingInfo->Id = baseElement->Id;
+		openingInfo->Name = baseElement->Name;
+		openingInfo->CategoryName = baseElement->CategoryName;
+
+		wallElementInfo->Openings->Add(openingInfo);
+	}
+	report->WallElemnts->Add(wallElementInfo);
+}
+
+void AuditWallOpeningCommand::PrepareType(Elements::WallElement^ wallElement) {
+	if (!report->WallTypes->ContainsKey(wallElement->Name)) {
+		MyDomain::WallOpening::WallTypeInfo^ wallTypeInfo = gcnew MyDomain::WallOpening::WallTypeInfo();
+		
+		report->WallTypes->Add(wallElement->Name, wallTypeInfo);
+	}
+	
+	report->WallTypes[wallElement->Name]->Name = wallElement->Name;
+	++report->WallTypes[wallElement->Name]->TotalWalls;
+	report->WallTypes[wallElement->Name]->TotalArea += wallElement->Area;
+
+	if (wallElement->HasOpening) {
+		report->WallTypes[wallElement->Name]->TotalOpenings += wallElement->CountOpening;
+		report->WallTypes[wallElement->Name]->TotalAreaWallsWithOpenings += wallElement->Area;
+
+		for each(Elements::BaseElement^ baseElement in wallElement->LogicChildren) {
+			MyDomain::WallOpening::OpeningInfo^ openingInfo = gcnew MyDomain::WallOpening::OpeningInfo();
+
+			openingInfo->Id = baseElement->Id;
+			openingInfo->Name = baseElement->Name;
+			openingInfo->CategoryName = baseElement->CategoryName;
+			
+			report->WallTypes[wallElement->Name]->Openings->Add(openingInfo);
+		}
+	} else {
+		report->WallTypes[wallElement->Name]->TotalAreaWallsWithoutOpenings += wallElement->Area;
+	}
 }
 
 void AuditWallOpeningCommand::Audit(List<Tuple<int, String^, String^>^>^ numFilterValue) {
@@ -167,104 +85,36 @@ void AuditWallOpeningCommand::Audit(List<Tuple<int, String^, String^>^>^ numFilt
 
 	List<Elements::WallElement^>^ wallElements = wallService->CollectByCategory(BuiltInCategory::OST_Walls, filtersValue, findParam);
 
-	dataTypeWallSummary_ = gcnew Dictionary<String^, AuditTypeWallSummary^>();
-	dataElementWall_ = gcnew List<AuditElementWall^>();
-
-	for each (Elements::WallElement^ we in wallElements) {
-
-		AuditElementWall^ auditElementWall = gcnew AuditElementWall();	
-		auditElementWall->Name = we->Name;
-		auditElementWall->Id = we->Id;
-		auditElementWall->CategoryName = we->CategoryName;
-		auditElementWall->Area = we->Area;
-		auditElementWall->CountOpening = we->CountOpening;
-
-		if (!dataTypeWallSummary_->ContainsKey(we->Name)) {
-			dataTypeWallSummary_->Add(we->Name, gcnew AuditTypeWallSummary());
-		}
-		
-		dataTypeWallSummary_[we->Name]->Name = we->Name;
-		++dataTypeWallSummary_[we->Name]->TotalWalls;
-		dataTypeWallSummary_[we->Name]->TotalArea += we->Area;
-
-		if (we->HasOpening) {
-			dataTypeWallSummary_[we->Name]->TotalOpening += we->CountOpening;
-			dataTypeWallSummary_[we->Name]->TotalAreaWallsWithOpenings += we->Area;
-
-			for each (Elements::BaseElement^ be in we->LogicChildren) {
-				DataOpening^ dataOpening = gcnew DataOpening();
-				dataOpening->Name = be->Name;
-				dataOpening->Id = be->Id;
-				dataOpening->CategoryName = be->CategoryName;
-			}
-
-		} else {
-			dataTypeWallSummary_[we->Name]->TotalAreaWallsWithoutOpenings += we->Area;
-		}
+	for each (Elements::WallElement^ wallElement in wallElements) {
+		PrepareElement(wallElement);
+		PrepareType(wallElement);
 	}
+
 	FillTable();
 
 	form_->MarkAuditFinished(true);
 }
 
 void AuditWallOpeningCommand::FillTable() {
-	for each (KeyValuePair<String^, AuditTypeWallSummary^> kvp in dataTypeWallSummary_) {
-		form_->AddRowTable(
+	for each (KeyValuePair<String^, MyDomain::WallOpening::WallTypeInfo^> kvp in report->WallTypes) {
+		form_->AddRowTable (
 			kvp.Value->Name,
 			kvp.Value->TotalWalls.ToString(),
-			kvp.Value->TotalArea.ToString("F2"),
-			kvp.Value->TotalAreaWallsWithOpenings.ToString("F2"),
-			kvp.Value->TotalAreaWallsWithoutOpenings.ToString("F2"),
-			kvp.Value->TotalOpening.ToString()
+			kvp.Value->TotalArea.ToString("F3"),
+			kvp.Value->TotalAreaWallsWithOpenings.ToString("F3"),
+			kvp.Value->TotalAreaWallsWithoutOpenings.ToString("F3"),
+			kvp.Value->TotalOpenings.ToString()
 		);
 	}
 }
 
-void AuditWallOpeningCommand::ExportPrepareData(ExportHtml::WallOpening::ReportModel^ repModel) {
-	for each (KeyValuePair<String^, AuditTypeWallSummary^> kvp in dataTypeWallSummary_) {
-		ExportHtml::WallOpening::ReportTypeWall^ reportTypeWall = gcnew ExportHtml::WallOpening::ReportTypeWall();
-
-		reportTypeWall->Name = kvp.Value->Name;
-		reportTypeWall->TotalWalls = kvp.Value->TotalWalls;
-		reportTypeWall->TotalArea = kvp.Value->TotalArea;
-		reportTypeWall->TotalOpening = kvp.Value->TotalOpening;
-		reportTypeWall->TotalAreaWallsWithOpenings = kvp.Value->TotalAreaWallsWithOpenings;
-		reportTypeWall->TotalAreaWallsWithoutOpenings = kvp.Value->TotalAreaWallsWithoutOpenings;
-
-		repModel->DataTypeWall->Add(reportTypeWall);
-	}
-
-	for each (AuditElementWall^ ew in dataElementWall_) {
-		ExportHtml::WallOpening::ReportElementWall^ reportElementWall = gcnew ExportHtml::WallOpening::ReportElementWall();
-		
-		reportElementWall->Name = ew->Name;
-		reportElementWall->Id = ew->Id;
-		reportElementWall->CategoryName = ew->CategoryName;
-		reportElementWall->Area = ew->Area;
-		reportElementWall->CountOpening = ew->CountOpening;
-		reportElementWall->DataOpenings = gcnew List<ExportHtml::WallOpening::DataOpening^>();
-		for each (DataOpening^ dataOpening in ew->DataOpenings) {
-			ExportHtml::WallOpening::DataOpening^ expDataOpening = gcnew ExportHtml::WallOpening::DataOpening();
-			expDataOpening->Id = dataOpening->Id;
-			expDataOpening->Name = dataOpening->Name;
-			expDataOpening->CategoryName = dataOpening->CategoryName;
-			reportElementWall->DataOpenings->Add(expDataOpening);
-		}
-		repModel->DataElementWall->Add(reportElementWall);
-	}
-}
-
 void AuditWallOpeningCommand::Export(String^ path) {
-	ExportHtml::WallOpening::ReportModel^ repModel = gcnew ExportHtml::WallOpening::ReportModel();
 
-	repModel->FilePath = doc_->PathName;
-	repModel->ProjectName = doc_->Title;
-	repModel->DataTimeStr = System::DateTime::Now.ToString("yyyy-MM-dd HH:mm:ss", System::Globalization::CultureInfo::InvariantCulture);
+	report->ProjectName = doc_->Title;
+	report->DateTimeStr = System::DateTime::Now.ToString("yyyy-MM-dd HH:mm:ss", System::Globalization::CultureInfo::InvariantCulture);
 	
-	ExportPrepareData(repModel);
-
-	ExportHtml::WallOpening::AuditWallOpeningExportHtml^ exp = gcnew ExportHtml::WallOpening::AuditWallOpeningExportHtml();
-	exp->SaveHtmlToFile(repModel, path);
+	ExportHtml::AuditWallOpeningExportHtml^ exp = gcnew ExportHtml::AuditWallOpeningExportHtml();
+	exp->SaveHtmlToFile(report, path);
 
 	form_->MarkAuditExportFinished(true);
 }
--- VPlug/Headers/Commands/AuditWallOpeningCommand.h
+++ VPlug/Headers/Commands/AuditWallOpeningCommand.h
@@ -2,6 +2,7 @@
 
 #include "../../Headers/UI/MyForms/FormAuditWallOpening.h"
 #include "../../Headers/Revit/Services/WallService.h"
+#include "../../Headers/Domain/WallOpeningDomain.h"
 #include "../../Headers/Export/html/AuditWallOpeningExportHtml.h"
 
 using namespace System;
@@ -15,122 +16,6 @@ using namespace Autodesk::Revit::Attributes;
 
 namespace Commands {
 
-public ref class DataOpening {
-private:
-	String^ name_;
-	int id_;
-	String^ categoryName_;
-
-public:
-	property String^ Name {
-		String^ get();
-		void set(String^ value);
-	};
-
-	property int Id {
-		int get();
-		void set(int value);
-	};
-
-	property String^ CategoryName {
-		String^ get();
-		void set(String^ value);
-	};
-};
-
-public ref class AuditElementWall {
-private:
-	String^ name_;
-	int id_;
-	String^ categoryName_;
-	double area_;
-	int countOpening_;
-	IList<DataOpening^>^ dataOpenings_;
-
-public:
-	property String^ Name {
-		String^ get();
-		void set(String^ value);
-	};
-
-	property int Id {
-		int get();
-		void set(int value);
-	};
-
-	property String^ CategoryName {
-		String^ get();
-		void set(String^ value);
-	};
-
-	property double Area {
-		double get();
-		void set(double value);
-	};
-
-	property int CountOpening {
-		int get();
-		void set(int value);
-	};
-
-	property IList<DataOpening^>^ DataOpenings {
-		IList<DataOpening^>^ get();
-		void set(IList<DataOpening^>^ value);
-	};
-};
-
-public ref class AuditTypeWallSummary {
-private:
-	String^ name_;
-	int totalWalls_;
-	double totalArea_;
-	int totalOpening_;
-	double totalAreaWallsWithOpenings_;
-	double totalAreaWallsWithoutOpenings_;
-	IList<DataOpening^>^ dataOpenings_;
-
-public:
-
-	AuditTypeWallSummary();
-
-	property String^ Name {
-		String^ get();
-		void set(String^ value);
-	};
-
-	property int TotalWalls {
-		int get();
-		void set(int value);
-	};
-
-	property double TotalArea {
-		double get();
-		void set(double value);
-	};
-
-	property int TotalOpening {
-		int get();
-		void set(int value);
-	};
-
-	property double TotalAreaWallsWithOpenings {
-		double get();
-		void set(double value);
-	};
-
-	property double TotalAreaWallsWithoutOpenings {
-		double get();
-		void set(double value);
-	};
-
-	property IList<DataOpening^>^ DataOpenings {
-		IList<DataOpening^>^ get();
-		void set(IList<DataOpening^>^ value);
-	};
-
-	Dictionary<String^, String^>^ filters_;
-};
-
 [Transaction(TransactionMode::Manual)]
 public ref class AuditWallOpeningCommand : public IExternalCommand {
 private:
@@ -140,14 +25,14 @@ private:
 	void SubscriptionEvent();
 
 	void Audit(List<Tuple<int, String^, String^>^>^ numFilterValue);
+	void PrepareElement(Elements::WallElement^ wallElement);
+	void PrepareType(Elements::WallElement^ wallElement);
 
 	void FillTable();
 
-	void ExportPrepareData(ExportHtml::WallOpening::ReportModel^ repModel);
 	void Export(String^ path);
 
-	Dictionary<String^, AuditTypeWallSummary^>^ dataTypeWallSummary_;
-	IList<AuditElementWall^>^ dataElementWall_;
+	MyDomain::WallOpening::WallOpeningReport^ report;
 
 public:
 	virtual Result Execute(ExternalCommandData^ commandData,
