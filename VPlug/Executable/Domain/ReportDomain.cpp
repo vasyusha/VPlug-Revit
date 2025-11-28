48e09e7490291e996b971d33aec2d5648b0e827a
vasyusha <vasyusha7@gmail.com>
2025-11-28 14:22:47 +0300

    add file ReportDomain.h/.cpp - хранит данные отчета, реализован Elements

---
+++ VPlug/Executable/Domain/ReportDomain.cpp
@@ -0,0 +1,94 @@
+﻿#include "../../Headers/Domain/ReportDomain.h"
+
+namespace MyDomain {
+
+namespace Reports {
+
+namespace Elements {
+
+MyDomain::Elements::Element^ ElementReport::Element::get() {
+	return element_;
+}
+
+void ElementReport::Element::set(MyDomain::Elements::Element^ value) {
+	element_ = value;
+}
+
+GroupReport::GroupReport() {
+	this->Elements = gcnew List<ElementReport^>();
+	this->CheckedParameters = gcnew List<String^>();
+};
+
+MyDomain::Parameters::Audit::Group^ GroupReport::Group::get() {
+	return group_;
+}
+
+void GroupReport::Group::set(MyDomain::Parameters::Audit::Group^ value) {
+	group_ = value;
+}
+
+IList<ElementReport^>^ GroupReport::Elements::get() {
+	return elements_;
+}
+
+void GroupReport::Elements::set(IList<ElementReport^>^ value) {
+	elements_ = value;
+}
+
+IList<String^>^ GroupReport::CheckedParameters::get() {
+	return checkedParameters_;
+}
+
+void GroupReport::CheckedParameters::set(IList<String^>^ value) {
+	checkedParameters_ = value;
+}
+
+ResultReport::ResultReport() {
+	this->Groups = gcnew List<GroupReport^>();
+}
+
+String^ ResultReport::FilePath::get() {
+	return filePath_;
+}
+
+void ResultReport::FilePath::set(String^ value) {
+	filePath_ = value;
+}
+
+String^ ResultReport::ProjectName::get() {
+	return projectName_;
+}
+
+void ResultReport::ProjectName::set(String^ value) {
+	projectName_ = value;
+}
+
+String^ ResultReport::DateTimeStr::get() {
+	return dateTimeStr_;
+}
+
+void ResultReport::DateTimeStr::set(String^ value) {
+	dateTimeStr_ = value;
+}
+
+IList<GroupReport^>^ ResultReport::Groups::get() {
+	return groups_;
+}
+
+void ResultReport::Groups::set(IList<GroupReport^>^ value) {
+	groups_ = value;
+}
+
+MyDomain::Parameters::Audit::Result^ ResultReport::Result::get() {
+	return result_;
+}
+
+void ResultReport::Result::set(MyDomain::Parameters::Audit::Result^ value) {
+	result_ = value;
+}
+
+}//namespace Elements
+
+}//namespace Reports
+
+}//namespace MyDomain
---
+++ VPlug/Headers/Domain/ReportDomain.h
@@ -0,0 +1,87 @@
+﻿#pragma once
+
+#include "ParameterDomain.h"
+#include "ElementDomain.h"
+
+using namespace System;
+using namespace System::Collections::Generic;
+
+namespace MyDomain {
+
+namespace Reports {
+
+namespace Elements {
+
+public ref class ElementReport {
+private:
+	MyDomain::Elements::Element^ element_;
+
+public:
+	property MyDomain::Elements::Element^ Element {
+		MyDomain::Elements::Element^ get();
+		void set(MyDomain::Elements::Element^ value);
+	};
+};
+
+public ref class GroupReport {
+private:
+	MyDomain::Parameters::Audit::Group^ group_;
+	IList<ElementReport^>^ elements_;
+	IList<String^>^ checkedParameters_;
+
+public:
+	GroupReport();
+
+	property MyDomain::Parameters::Audit::Group^ Group {
+		MyDomain::Parameters::Audit::Group^ get();
+		void set(MyDomain::Parameters::Audit::Group^ value);
+	};
+	property IList<ElementReport^>^ Elements {
+		IList<ElementReport^>^ get();
+		void set(IList<ElementReport^>^ value);
+	};
+	property IList<String^>^ CheckedParameters {
+		IList<String^>^ get();
+		void set(IList<String^>^ value);
+	};
+
+};
+
+public ref class ResultReport {
+private:
+	String^ filePath_;
+	String^ projectName_;
+	String^ dateTimeStr_;
+	IList<GroupReport^>^ groups_;
+	MyDomain::Parameters::Audit::Result^ result_;
+
+public:
+	ResultReport();
+
+	property String^ FilePath {
+		String^ get();
+		void set(String^ value);
+	};
+	property String^ ProjectName {
+		String^ get();
+		void set(String^ value);
+	};
+	property String^ DateTimeStr {
+		String^ get();
+		void set(String^ value);
+	};
+	property IList<GroupReport^>^ Groups {
+		IList<GroupReport^>^ get();
+		void set(IList<GroupReport^>^ value);
+	};
+	property MyDomain::Parameters::Audit::Result^ Result {
+		MyDomain::Parameters::Audit::Result^ get();
+		void set(MyDomain::Parameters::Audit::Result^ value);
+	};
+};
+
+}//namespace Elements 
+
+}//namespace Reports
+
+}//namespace MyDomain
