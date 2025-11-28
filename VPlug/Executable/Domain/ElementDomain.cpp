01cda365211f3f087ffbd2b91a66ae7bba45f58c
vasyusha <vasyusha7@gmail.com>
2025-11-28 14:21:04 +0300

    add file ElementDomain.h/.cpp - хранит данные о элементе

---
+++ VPlug/Executable/Domain/ElementDomain.cpp
@@ -0,0 +1,69 @@
+﻿#include "../../Headers/Domain/ElementDomain.h"
+
+namespace MyDomain {
+
+namespace Elements {
+
+Element::Element() {
+	this->Param = gcnew List<Parameters::Parameter^>();
+}
+
+int Element::Id::get() {
+	return id_;
+}
+
+void Element::Id::set(int value) {
+	id_ = value;
+}
+
+String^ Element::UniqueId::get() {
+	return uniqueId_;
+}
+
+void Element::UniqueId::set(String^ value) {
+	uniqueId_ = value;
+}
+
+String^ Element::Name::get() {
+	return name_;
+}
+
+void Element::Name::set(String^ value) {
+	name_ = value;
+}
+
+String^ Element::CategoryName::get() {
+	return categoryName_;
+}
+
+void Element::CategoryName::set(String^ value) {
+	categoryName_ = value;
+}
+
+int Element::BuiltInCategory::get() {
+	return builtInCategory_;
+}
+
+void Element::BuiltInCategory::set(int value) {
+	builtInCategory_ = value;
+}
+
+String^ Element::BuiltInCategoryName::get() {
+	return builtInCategoryName_;
+}
+
+void Element::BuiltInCategoryName::set(String^ value) {
+	builtInCategoryName_ = value;
+}
+
+IList<Parameters::Parameter^>^ Element::Param::get() {
+	return parameters_;
+}
+
+void Element::Param::set(IList<Parameters::Parameter^>^ value) {
+	parameters_ = value;
+}
+
+}//namespace Elements
+
+}//namespace MyDomain
---
+++ VPlug/Headers/Domain/ElementDomain.h
@@ -0,0 +1,58 @@
+﻿#pragma once
+
+#include "ParameterDomain.h"
+
+using namespace System;
+using namespace System::Collections::Generic;
+
+namespace MyDomain {
+
+namespace Elements {
+
+public ref class Element {
+private:
+	int id_;
+	String^ uniqueId_;
+	String^ name_;
+	String^ categoryName_;
+	int builtInCategory_;
+	String^ builtInCategoryName_;
+	IList<Parameters::Parameter^>^ parameters_;
+
+public:
+	Element();
+
+	property int Id {
+		int get();
+		void set(int value);
+	};
+	property String^ UniqueId {
+		String^ get();
+		void set(String^ value);
+	};
+	property String^ Name {
+		String^ get();
+		void set(String^ value);
+	};
+	property String^ CategoryName {
+		String^ get();
+		void set(String^ value);
+	};
+	property int BuiltInCategory {
+		int get();
+		void set(int value);
+	};
+	property String^ BuiltInCategoryName {
+		String^ get();
+		void set(String^ value);
+	};
+	property IList<Parameters::Parameter^>^ Param {
+		IList<Parameters::Parameter^>^ get();
+		void set(IList<Parameters::Parameter^>^ value);
+	};
+};
+
+}//namespace Elements
+
+}//namespace MyDomain
+
