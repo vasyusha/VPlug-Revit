02565a7dd87c9b912251d298090da06796ea95f8
vasyusha <vasyusha7@gmail.com>
2025-12-03 16:21:16 +0300

    feat(ElementDomain): перенес сюда Audit из ParameterDomain

--- VPlug/Executable/Domain/ElementDomain.cpp
+++ VPlug/Executable/Domain/ElementDomain.cpp
@@ -5,7 +5,7 @@ namespace MyDomain {
 namespace Elements {
 
 Element::Element() {
-	this->Param = gcnew List<Parameters::Parameter^>();
+	this->Parameters = gcnew List<MyDomain::Parameters::Parameter^>();
 }
 
 int Element::Id::get() {
@@ -56,13 +56,320 @@ void Element::BuiltInCategoryName::set(String^ value) {
 	builtInCategoryName_ = value;
 }
 
-IList<Parameters::Parameter^>^ Element::Param::get() {
+IList<MyDomain::Parameters::Parameter^>^ Element::Parameters::get() {
 	return parameters_;
 }
 
-void Element::Param::set(IList<Parameters::Parameter^>^ value) {
+void Element::Parameters::set(IList<MyDomain::Parameters::Parameter^>^ value) {
 	parameters_ = value;
 }
+
+double WallElement::Area::get() {
+	return area_;
+}
+
+void WallElement::Area::set(double value) {
+	area_ = value;
+}
+
+bool WallElement::HasOpening::get() {
+	return hasOpening_;
+}
+
+void WallElement::HasOpening::set(bool value) {
+	hasOpening_ = value;
+}
+
+int WallElement::CountOpenings::get() {
+	return countOpenings_;
+}
+
+void WallElement::CountOpenings::set(int value) {
+	countOpenings_ = value;
+}
+
+IList<Element^>^ WallElement::LogicChildrenOpenings::get() {
+	return logicChildrenOpenings_;
+}
+
+void WallElement::LogicChildrenOpenings::set(IList<Element^>^ value) {
+	logicChildrenOpenings_ = value;
+}
+
+namespace AuditParameters {
+
+MyDomain::Elements::Element^ AuditElement::Element::get() {
+	return element_;
+}
+
+void AuditElement::Element::set(MyDomain::Elements::Element^ value) {
+	element_ = value;
+}
+
+bool AuditElement::Pass::get() {
+	return pass_;
+}
+
+void AuditElement::Pass::set(bool value) {
+	pass_ = value;
+}
+
+int AuditElement::TotalParams::get() {
+	return totalParams_;
+}
+
+void AuditElement::TotalParams::set(int value) {
+	totalParams_ = value;
+}
+
+int AuditElement::PassParams::get() {
+	return passParams_;
+}
+
+void AuditElement::PassParams::set(int value) {
+	passParams_ = value;
+}
+
+int AuditElement::FailParams::get() {
+	return failParams_;
+}
+
+void AuditElement::FailParams::set(int value) {
+	failParams_ = value;
+}
+
+int AuditElement::FilledParams::get() {
+	return filledParams_;
+}
+
+void AuditElement::FilledParams::set(int value) {
+	filledParams_ = value;
+}
+
+int AuditElement::EmptyParams::get() {
+	return emptyParams_;
+}
+
+void AuditElement::EmptyParams::set(int value) {
+	emptyParams_ = value;
+}
+
+int AuditElement::MissingParams::get() {
+	return missingParams_;
+}
+
+void AuditElement::MissingParams::set(int value) {
+	missingParams_ = value;
+}
+
+int AuditElement::Percent::get() {
+	return percent_;
+}
+
+void AuditElement::Percent::set(int value) {
+	if (value < 0) value = 0;
+	if (value > 100) value = 100;
+	percent_ = value;
+}
+
+AuditGroup::AuditGroup() {
+	this->Elements = gcnew List<AuditElement^>();
+	this->CheckedParameters = gcnew List<String^>();
+}
+
+String^ AuditGroup::Name::get() {
+	return name_;
+}
+
+void AuditGroup::Name::set(String^ value) {
+	name_ = value;
+}
+
+bool AuditGroup::Pass::get() {
+	return pass_;
+}
+
+void AuditGroup::Pass::set(bool value) {
+	pass_ = value;
+}
+
+int AuditGroup::TotalElements::get() {
+	return totalElements_;
+}
+
+void AuditGroup::TotalElements::set(int value) {
+	totalElements_ = value;
+}
+
+int AuditGroup::PassElements::get() {
+	return passElements_;
+}
+
+void AuditGroup::PassElements::set(int value) {
+	passElements_ = value;
+}
+
+int AuditGroup::FailElements::get() {
+	return failElements_;
+}
+
+void AuditGroup::FailElements::set(int value) {
+	failElements_ = value;
+}
+
+int AuditGroup::TotalParams::get() {
+	return totalParams_;
+}
+
+void AuditGroup::TotalParams::set(int value) {
+	totalParams_ = value;
+}
+
+int AuditGroup::PassParams::get() {
+	return passParams_;
+}
+
+void AuditGroup::PassParams::set(int value) {
+	passParams_ = value;
+}
+
+int AuditGroup::FailParams::get() {
+	return failParams_;
+}
+
+void AuditGroup::FailParams::set(int value) {
+	failParams_ = value;
+}
+
+int AuditGroup::EmptyParams::get() {
+	return emptyParams_;
+}
+
+void AuditGroup::EmptyParams::set(int value) {
+	emptyParams_ = value;
+}
+
+int AuditGroup::MissingParams::get() {
+	return missingParams_;
+}
+
+void AuditGroup::MissingParams::set(int value) {
+	missingParams_ = value;
+}
+
+int AuditGroup::Percent::get() {
+	return percent_;	
+}
+
+void AuditGroup::Percent::set(int value) {
+	if (value < 0) value = 0;
+	if (value > 100) value = 100;
+	percent_ = value;
+}
+
+IList<AuditElement^>^ AuditGroup::Elements::get() {
+	return elements_;
+}
+
+void AuditGroup::Elements::set(IList<AuditElement^>^ value) {
+	elements_ = value;
+}
+
+IList<String^>^ AuditGroup::CheckedParameters::get() {
+	return checkedParameters_;
+}
+
+void AuditGroup::CheckedParameters::set(IList<String^>^ value) {
+	checkedParameters_ = value;
+}
+
+AuditResult::AuditResult() {
+	this->Groups = gcnew List<AuditGroup^>();
+}
+
+bool AuditResult::Pass::get() {
+	return pass_;
+}
+
+void AuditResult::Pass::set(bool value) {
+	pass_ = value;
+}
+
+int AuditResult::TotalGroups::get() {
+	return totalGroups_;
+}
+
+void AuditResult::TotalGroups::set(int value) {
+	totalGroups_ = value;
+}
+
+int AuditResult::PassGroups::get() {
+	return passGroups_;
+}
+
+void AuditResult::PassGroups::set(int value) {
+	passGroups_ = value;
+}
+
+int AuditResult::FailGroup::get() {
+	return failGrpups_;
+}
+
+void AuditResult::FailGroup::set(int value) {
+	failGrpups_ = value;
+}
+
+int AuditResult::ReqElementPass::get() {
+	return reqElementPass_;
+}
+
+void AuditResult::ReqElementPass::set(int value) {
+	reqElementPass_ = value;
+}
+
+int AuditResult::ReqElementTotal::get() {
+	return reqElementTotal_;
+}
+
+void AuditResult::ReqElementTotal::set(int value) {
+	reqElementTotal_ = value;
+}
+
+int AuditResult::ReqParamPass::get() {
+	return reqParamPass_;
+}
+
+void AuditResult::ReqParamPass::set(int value) {
+	reqParamPass_ = value;
+}
+
+int AuditResult::ReqParamTotal::get() {
+	return reqParamTotal_;
+}
+
+void AuditResult::ReqParamTotal::set(int value) {
+	reqParamTotal_ = value;
+}
+
+int AuditResult::Percent::get() {
+	return percent_;
+}
+
+void AuditResult::Percent::set(int value) {
+	if (value < 0) value = 0;
+	if (value > 100) value = 100;
+	percent_ = value;
+}
+
+IList<AuditGroup^>^ AuditResult::Groups::get() {
+	return groups_;
+}
+
+void AuditResult::Groups::set(IList<AuditGroup^>^ value) {
+	groups_ = value;
+}
+
+}//namespace AuditParameters
 
 }//namespace Elements
 
--- VPlug/Headers/Domain/ElementDomain.h
+++ VPlug/Headers/Domain/ElementDomain.h
@@ -10,7 +10,7 @@ namespace MyDomain {
 namespace Elements {
 
 public ref class Element {
-private:
+protected:
 	int id_;
 	String^ uniqueId_;
 	String^ name_;
@@ -46,11 +46,225 @@ public:
 		String^ get();
 		void set(String^ value);
 	};
-	property IList<Parameters::Parameter^>^ Param {
-		IList<Parameters::Parameter^>^ get();
-		void set(IList<Parameters::Parameter^>^ value);
+	property IList<MyDomain::Parameters::Parameter^>^ Parameters {
+		IList<MyDomain::Parameters::Parameter^>^ get();
+		void set(IList<MyDomain::Parameters::Parameter^>^ value);
 	};
 };
+
+public ref class WallElement : public Element {
+private:
+	double area_;
+	bool hasOpening_;
+	int countOpenings_;
+	/*Логические дочерние элементы проёмов, включая FindInserts(addRectOpenings, includeShadows)*/
+	IList<Element^>^ logicChildrenOpenings_;
+	
+public:
+	property double Area {
+		double get();
+		void set(double value);
+	};
+	property bool HasOpening {
+		bool get();
+		void set(bool value);
+	};
+	property int CountOpenings {
+		int get();
+		void set(int value);
+	};
+	property IList<Element^>^ LogicChildrenOpenings {
+		IList<Element^>^ get();
+		void set(IList<Element^>^ value);
+	};
+};
+
+namespace AuditParameters {
+
+public ref class AuditElement {
+private:
+	Element^ element_;
+	bool pass_;
+	int totalParams_;
+	int passParams_;
+	int failParams_;
+	int filledParams_;
+	int emptyParams_;
+	int missingParams_;
+	int percent_;
+
+public:
+	property Element^ Element {
+		MyDomain::Elements::Element^ get();
+		void set(MyDomain::Elements::Element^ value);
+	};
+	property bool Pass{
+		bool get();
+		void set(bool value);
+	};
+	property int TotalParams {
+		int get();
+		void set(int value);
+	};
+	property int PassParams {
+		int get();
+		void set(int value);
+	};
+	property int FailParams {
+		int get();
+		void set(int value);
+	};
+	property int FilledParams {
+		int get();
+		void set(int value);
+	};
+	property int EmptyParams {
+		int get();
+		void set(int value);
+	};
+	property int MissingParams {
+		int get();
+		void set(int value);
+	};
+	property int Percent {
+		int get();
+		void set(int value);
+	};
+};
+
+public ref class AuditGroup {
+private:
+	String^ name_;
+	bool pass_;
+	int totalElements_;
+	int passElements_;
+	int failElements_;
+	int totalParams_;
+	int passParams_;
+	int failParams_;
+	int emptyParams_;
+	int missingParams_;
+	int percent_;
+	IList<AuditElement^>^ elements_;
+	IList<String^>^ checkedParameters_;
+
+public:
+	AuditGroup();
+
+	property String^ Name {
+		String^ get();
+		void set(String^ value);
+	};
+	property bool Pass {
+		bool get();
+		void set(bool value);
+	};
+	property int TotalElements {
+		int get();
+		void set(int value);
+	};
+	property int PassElements {
+		int get();
+		void set(int value);
+	};
+	property int FailElements {
+		int get();
+		void set(int value);
+	};
+	property int TotalParams {
+		int get();
+		void set(int value);
+	};
+	property int PassParams {
+		int get();
+		void set(int value);
+	};
+	property int FailParams {
+		int get();
+		void set(int value);
+	};
+	property int EmptyParams {
+		int get();
+		void set(int value);
+	};
+	property int MissingParams {
+		int get();
+		void set(int value);
+	};
+	property int Percent {
+		int get();
+		void set(int value);
+	};
+	property IList<AuditElement^>^ Elements {
+		IList<AuditElement^>^ get();
+		void set(IList<AuditElement^>^ value);
+	};
+	property IList<String^>^ CheckedParameters {
+		IList<String^>^ get();
+		void set(IList<String^>^ value);
+	};
+};
+
+public ref class AuditResult {
+private:
+	bool pass_;
+	int totalGroups_;
+	int passGroups_;
+	int failGrpups_;
+	//Req = requirements
+	int reqElementPass_; //Требования к элементу ок
+	int reqElementTotal_; //Требования к элементу всего
+	int reqParamPass_; //Требования к параметру ок
+	int reqParamTotal_; //Требования к параметру всего
+	int percent_;
+	IList<AuditGroup^>^ groups_;	
+
+public:
+	AuditResult();	
+
+	property bool Pass {
+		bool get();
+		void set(bool value);
+	};
+	property int TotalGroups {
+		int get();
+		void set(int value);
+	};
+	property int PassGroups {
+		int get();
+		void set(int value);
+	};
+	property int FailGroup {
+		int get();
+		void set(int value);
+	};
+	property int ReqElementPass{
+		int get();
+		void set(int value);
+	};
+	property int ReqElementTotal {
+		int get();
+		void set(int value);
+	};
+	property int ReqParamPass {
+		int get();
+		void set(int value);
+	};
+	property int ReqParamTotal {
+		int get();
+		void set(int value);
+	};
+	property int Percent {
+		int get();
+		void set(int value);
+	};
+	property IList<AuditGroup^>^ Groups {
+		IList<AuditGroup^>^ get();
+		void set(IList<AuditGroup^>^ value);
+	};
+};
+
+}//namespace AuditParameters
 
 }//namespace Elements
 
